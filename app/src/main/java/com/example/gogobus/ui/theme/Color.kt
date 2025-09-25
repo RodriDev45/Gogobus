012f87ea5f9276444ba4b6d0c236ef9675146e37
marco <marco.nurena@tecsup.edu.pe>
2025-09-25 17:25:06 -0500

    addtheme

--- app/src/main/java/com/example/gogobus/ui/theme/Color.kt
+++ app/src/main/java/com/example/gogobus/ui/theme/Color.kt
@@ -2,6 +2,21 @@ package com.example.gogobus.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val OrangeGogobus = Color(0xFFF5951F)
+
+val White = Color(0xFFFFFFFF)
+val BackgroundLight = Color(0xFFF0F2F4)
+val SurfaceLine = Color(0xFFE1E5EA)
+val TextBlack = Color(0xFF000000)
+val TextGrayDark = Color(0xFF64748B)
+val TextGrayMedium = Color(0xFF67768D)
+val IconActive = Color(0xFF005566)
+val GrayIconInactive = Color(0xFFA6B0BF)
+val PlaceholderGray = Color(0xFFACB5C3)
+
+
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
