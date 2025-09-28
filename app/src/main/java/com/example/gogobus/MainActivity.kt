91475ad5b84226592b6b5c0c2ac75c0321cc471d
valentino <126835108+Valen89hh@users.noreply.github.com>
2025-09-28 16:16:29 -0500

    add splash screen

--- app/src/main/java/com/example/gogobus/MainActivity.kt
+++ app/src/main/java/com/example/gogobus/MainActivity.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.gogobus.navigation.AppNavHost
 import com.example.gogobus.ui.theme.GogobusTheme
 import dagger.hilt.android.AndroidEntryPoint
@@ -16,6 +17,7 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
